c6993e287ae67dbc730742fdf071331c6483f942
André Pereira <andrepereira97@yahoo.com>
2023-09-23 18:57:48 -0300

    :sparkles: add method to get a value based on enum

--- src/EnumDemo.java
+++ src/EnumDemo.java
@@ -2,7 +2,16 @@ package src;
 
 public class EnumDemo {
   public static void main(String[] args) {
-    System.out.println("Level: " + Level.INFO);
+    System.out.println("Level: " + getLevelValue(Level.UNKNOWN));
+  }
+
+  public static int getLevelValue(Level level) {
+    return switch (level) {
+      case INFO -> 1;
+      case WARNING -> 2;
+      case ERROR -> 3;
+      default -> 0;
+    };
   }
 
   enum Level {
