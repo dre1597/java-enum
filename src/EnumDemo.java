4c9c0ce6ceeeedad18d6180e0618dd5842d99f26
André Pereira <andrepereira97@yahoo.com>
2023-09-23 19:05:08 -0300

    :sparkles: improve example, using new values at enum constructor

--- src/EnumDemo.java
+++ src/EnumDemo.java
@@ -2,24 +2,40 @@ package src;
 
 public class EnumDemo {
   public static void main(String[] args) {
-    System.out.println("Level: " + Level.UNKNOWN.getValue());
+    for (Level level : Level.values()) {
+      System.out.println("Level " + level.name()
+      + "\n\tSeverity: " + level.getSeverity()
+      + "\n\tLabel: " + level.getLabel()
+      + "\n\tColor: " + level.getColor());
+    }
   }
 
 
   enum Level {
-    INFO(1),
-    WARNING(2),
-    ERROR(3),
-    UNKNOWN(0);
+    INFO(1, "INFO", 0x00AA00),
+    WARNING(2, "WARNING", 0xFFA500),
+    ERROR(3, "ERROR", 0xA30000);
 
-    private final int value;
+    private final int severity;
+    private final String label;
+    private final int color;
 
-    Level(int value) {
-      this.value = value;
+    Level(int severity, String label, int color) {
+      this.severity = severity;
+      this.label = label;
+      this.color = color;
     }
 
-    public int getValue() {
-      return value;
+    public int getSeverity() {
+      return severity;
+    }
+
+    public String getLabel() {
+      return label;
+    }
+
+    public int getColor() {
+      return color;
     }
   }
 }
