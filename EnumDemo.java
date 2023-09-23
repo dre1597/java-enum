f1740f1c534e1fb3ed7b3b2b6b7cb020f7e66414
André Pereira <andrepereira97@yahoo.com>
2023-09-23 18:45:56 -0300

    :tada: init project

---
+++ EnumDemo.java
@@ -0,0 +1,12 @@
+public class EnumDemo {
+  public static void main(String[] args) {
+    System.out.println("Level: " + Level.INFO);
+  }
+
+  enum Level {
+    INFO,
+    WARNING,
+    ERROR,
+    UNKNOWN
+  }
+}
